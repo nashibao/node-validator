387cf16bf8b88a5e2d95bb91ca4cad16c76c4b02
nashibao <nashibao@gmail.com>
2013-06-25 16:02:41 +0900

    add jp err localization

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.Validator = node_validator.Validator;
 exports.ValidatorError = node_validator.ValidatorError;
 exports.Filter = require('./lib/filter').Filter;
 exports.validators = require('./lib/validators');
-exports.defaultError = require('./lib/defaultError');
+exports.defaultError = require('./lib/defaultError')();
 
 exports.entities = require('./lib/entities');
 
@@ -12,9 +12,13 @@ exports.entities = require('./lib/entities');
 exports.sanitize = exports.convert = function(str) {
     var filter = new exports.Filter();
     return filter.sanitize(str);
-}
+};
 
 exports.check = exports.validate = exports.assert = function(str, fail_msg) {
     var validator = new exports.Validator();
     return validator.check(str, fail_msg);
-}
+};
+
+exports.localization = function(locale) {
+  exports.defaultError = require('./lib/defaultError')(locale);
+};
